a35675acac914516c1ceb4c40343a24d3abf487c
Karol Guzikowski <karolguzikowski@gmail.com>
2020-10-22 21:27:15 +0200

    removed comment and changed .gitignore

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter, SimpleRouter
+
+if settings.DEBUG:
+    router = DefaultRouter()
+else:
+    router = SimpleRouter()
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
+    path("api/", include(router.urls)),
 ] + static(settings.MEDIA_URL)
 
 
