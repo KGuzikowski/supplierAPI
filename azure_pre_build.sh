5d6428a682d53b6012ef0bc1ec246f0213c94ef0
Karol Guzikowski <karolguzikowski@gmail.com>
2020-10-23 13:45:07 +0200

    removed sudo from prebuild script and added installing requirements there

--- azure_pre_build.sh
+++ azure_pre_build.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
-sudo curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-sudo curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
-sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install msodbcsql17
+apt-get update
+ACCEPT_EULA=Y apt-get install msodbcsql17 -y
+
+pip install -U pip
+pip install -r ./requirements/prod.txt
