8835bfbb53cc7c0cc7d3e7fdc028eb921fd74f76
Karol Guzikowski <karolguzikowski@gmail.com>
2020-11-05 22:54:47 +0100

    finished register logic and remove .idea from repo

--- supplierapi/apps/myAuth/urls.py
+++ supplierapi/apps/myAuth/urls.py
@@ -2,14 +2,16 @@ from django.conf import settings
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from .views import CustomAuthToken
+from .views import CustomAuthToken, UserViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
 else:
     router = SimpleRouter()
 
+router.register("users", UserViewSet)
+
 urlpatterns = [
     path("", include(router.urls)),
-    path("authenticate", CustomAuthToken.as_view()),
+    path("authenticate/", CustomAuthToken.as_view()),
 ]
--- supplierapi/apps/myAuth/views.py
+++ supplierapi/apps/myAuth/views.py
@@ -2,6 +2,11 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 
+from supplierapi.utils.views import CustomViewSet
+
+from .models import User
+from .serializers import UserDetailSerializer, UserListSerializer, UserUpdateSerializer
+
 
 class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
@@ -15,3 +20,21 @@ class CustomAuthToken(ObtainAuthToken):
                 "user_id": str(user.pk),
             }
         )
+
+
+class UserViewSet(CustomViewSet):
+    """The viewset for the User model."""
+
+    queryset = User.objects.all()
+
+    # mapping serializer into the action
+    serializer_classes = {
+        "list": UserListSerializer,
+        "retrieve": UserDetailSerializer,
+        "update": UserUpdateSerializer,
+        "create": UserDetailSerializer,
+    }
+    default_serializer_class = UserListSerializer
+
+    def get_serializer_class(self):
+        return self.serializer_classes.get(self.action, self.default_serializer_class)
